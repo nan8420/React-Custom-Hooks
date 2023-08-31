3cfee622c5def9100a9bce76f2462a46a7960452
Kim Gun Hee <nan8420@gmail.com>
2023-08-31 16:42:39 +0900

    Feat: useInput 훅스 추가

--- src/Pages/UseInputPage/index.js
+++ src/Pages/UseInputPage/index.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { useInput } from "./useInput";
+
+function displayMessage(message) {
+  alert(message);
+}
 
 const UseInputPage = () => {
+  const [inputValue, handleChange, handleSubmit] = useInput("", displayMessage);
+
+  const [inputValue2, handleChange2, handleSubmit2] = useInput(
+    "",
+    displayMessage
+  );
+
   return (
     <div>
-      <h1>UseInputPage</h1>
+      <h1>useInput</h1>
+      <input value={inputValue} onChange={handleChange} />
+      <button onClick={handleSubmit}>확인</button>
+
+      <input value={inputValue2} onChange={handleChange2} />
+      <button onClick={handleSubmit2}>확인</button>
     </div>
   );
 };
